acd1321c052f9b3e1a9161a6563ba8b48b4cdf05
Han <jiaqi11.han@nttdatabj.com.cn>
2025-03-03 15:40:30 +0800

    conf change1

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 # main.tf
 terraform {
   required_version = ">= 1.5.0"
-  backend "remote" {  # 使用Terraform Cloud作?后端
+  backend "remote" {  # 使用Terraform Cloud作后端
     hostname     = "app.terraform.io"
-    organization = "<YOUR_TF_CLOUD_ORG_NAME>"
+    organization = "yumigaizi-org"
     workspaces {
-      name = "ec2-test-dev"  # ??Terraform Cloud中的Workspace名称
+      name = "terraform-yumi-cloud"  # Terraform Cloud中的Workspace名称
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  ami           = "ami-0d60652e0357ad94f"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   tags = {
-    Name = "TestInstance"
+    Name = "test_ec2_create_by_terraform_cloud"
   }
 }
