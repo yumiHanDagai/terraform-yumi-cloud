982a51d0cff02c637bdbc335ef5a62b3e3c8dcd3
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-03-05 15:14:35 +0800

    Update main.tf

--- deploy/aws/main.tf
+++ deploy/aws/main.tf
@@ -1,3 +1,10 @@
+variable "user_name" {}
+variable "policy_name" {}
+variable "linux_instance_type" {}
+variable "linux_instance_name" {}
+variable "windows_instance_type" {}
+variable "windows_instance_name" {}
+
 module "ec2_linux" {
   source = "./ec2/linux"
 }
