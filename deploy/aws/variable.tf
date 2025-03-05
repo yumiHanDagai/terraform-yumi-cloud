f4eef88a2140e76de33da5254e0c69f608f504ec
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-03-05 15:42:34 +0800

    Update variable.tf

--- deploy/aws/variable.tf
+++ deploy/aws/variable.tf
@@ -1,18 +1,24 @@
 variable "linux_instance_type" {
   type = string
+  default = "t2.micro"
 }
 variable "linux_instance_name" {
   type = string
+  default = "fail_linux_tfc"
 }
 variable "windows_instance_type" {
   type = string
+  default = "t2.small"
 }
 variable "windows_instance_name" {
   type = string
+  default = "fail_windows_tfc"
 }
 variable "user_name" {
   type = string
+  default = "fail_user"
 }
 variable "policy_name" {
   type = string
+  default = "fail_policy"
 }
