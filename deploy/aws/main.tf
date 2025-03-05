ae7f7844a2d370496b173152d8e9a36d17d02d27
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-03-05 16:08:41 +0800

    Update main.tf

--- deploy/aws/main.tf
+++ deploy/aws/main.tf
@@ -4,18 +4,18 @@ module "ec2_linux" {
   linux_instance_name = var.linux_instance_name
 }
 
-module "ec2_windows" {
-  source = "./ec2/windows"
-  windows_instance_type = var.windows_instance_type
-  windows_instance_name = var.windows_instance_name
-}
+#module "ec2_windows" {
+#  source = "./ec2/windows"
+#  windows_instance_type = var.windows_instance_type
+#  windows_instance_name = var.windows_instance_name
+#}
 
-module "iam_user" {
-  source = "./iam/user"
-  user_name = var.user_name
-}
+#module "iam_user" {
+#  source = "./iam/user"
+#  user_name = var.user_name
+#}
 
-module "iam_custompolicy" {
-  source = "./iam/custompolicy"
-  policy_name = var.policy_name
-}
+#module "iam_custompolicy" {
+#  source = "./iam/custompolicy"
+#  policy_name = var.policy_name
+#}
