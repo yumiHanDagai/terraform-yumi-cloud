1ab959546040584047725bb2f3d8b88dff8d553b
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-03-05 15:29:52 +0800

    Create variable.tf

---
+++ deploy/aws/variable.tf
@@ -0,0 +1,6 @@
+variable "linux_instance_type" {}
+variable "linux_instance_name" {}
+variable "windows_instance_type" {}
+variable "windows_instance_name" {}
+variable "user_name" {}
+variable "policy_name" {}
