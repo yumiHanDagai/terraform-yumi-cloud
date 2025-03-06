e4b0e822f1b0bff647416e91030146b58c294c2d
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-03-06 10:22:46 +0800

    Update main.tf

--- deploy/aws/ec2/linux/main.tf
+++ deploy/aws/ec2/linux/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "linux_example" {
-  ami           = "ami-09a73d1fbb2515f951"
+  ami           = "ami-09a73d1fbb2515f95"
   instance_type = var.linux_instance_type
   subnet_id     = "subnet-07b73439a9be9b6b4"
   vpc_security_group_ids = ["sg-0477797d3a1998110"]
