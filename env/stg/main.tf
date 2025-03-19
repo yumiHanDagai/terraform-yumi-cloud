4674251b0b2790a0768818be6ae1888cda8721da
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-03-19 14:00:18 +0800

    Update main.tf

--- env/stg/main.tf
+++ env/stg/main.tf
@@ -19,3 +19,63 @@ module "iam_custompolicy" {
   source = "../../deploy/aws/iam/custompolicy"
   policy_name = var.policy_name
 }
+
+variable "existing_bucket" {
+  default = "ndit-skillup-bedrock-video-trans-s3b"
+}
+
+# 上传本地脚本到现有 S3 存储桶
+resource "aws_s3_object" "glue_script" {
+  bucket = var.existing_bucket
+  key    = "scripts/glue-demo/script.py"
+  source = "${path.module}/scripts/script.py"
+  etag   = filemd5("${path.module}/scripts/script.py")
+}
+
+# 精简版 Glue 执行角色
+resource "aws_iam_role" "glue_role" {
+  name = "glue_demo_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "glue.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  inline_policy {
+    name = "s3_access"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [{
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = "arn:aws:s3:::${var.existing_bucket}/*"
+      }]
+    })
+  }
+}
+
+resource "aws_glue_job" "demo" {
+  name         = "no_s3_creation_demo_job"
+  role_arn     = aws_iam_role.glue_role.arn
+  glue_version = "4.0"
+
+  command {
+    name            = "pythonshell"
+    script_location = "s3://${var.existing_bucket}/${aws_s3_object.glue_script.key}"
+  }
+
+  default_arguments = {
+    "--job-language"        = "python"
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--enable-metrics"      = ""
+  }
+
+  timeout = 5
+}
