df76ac1ff77dc50c8510edae5cba33c08c4cd0a1
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-03-19 15:01:29 +0800

    Update providers.tf

--- env/stg/providers.tf
+++ env/stg/providers.tf
@@ -11,6 +11,5 @@ terraform {
 provider "aws" {
   assume_role_with_web_identity {
     role_arn          = "arn:aws:iam::138184589409:role/tfc-test-role-20250317"
-    session_name      = "terraform-session"
   }
 }
