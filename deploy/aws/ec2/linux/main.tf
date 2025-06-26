5b1988817f79f19a9df375dc12ba3621e76a959c
yumiHanDagai <124324378+yumiHanDagai@users.noreply.github.com>
2025-06-26 10:56:50 +0800

    test7

--- deploy/aws/ec2/linux/main.tf
+++ deploy/aws/ec2/linux/main.tf
@@ -2,7 +2,7 @@
   ami           = "ami-09a73d1fbb2515f95"
   instance_type = var.linux_instance_type
   subnet_id     = "subnet-07b73439a9be9b6b4"
-  vpc_security_group_ids = ["sg-0477797d3a1998110"]  
+  vpc_security_group_ids = ["sg-0477797d3a1998110"] 
   tags = {
     Name = var.linux_instance_name
   }
